2425cfc5cf007bc2d9146ce965abdebed28f1042
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2025-06-16 14:37:30 -0400

    Migrate from `tsup` to `tsdown` (#7)

--- eslint.config.js
+++ eslint.config.js
@@ -2,19 +2,14 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-// @ts-expect-error -- no types
 import * as pluginImport from 'eslint-plugin-import';
 
 export default tseslint.config(
-  {
-    ignores: ['**/node_modules', 'dist'],
-  },
+  { ignores: ['**/node_modules', 'dist'] },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    plugins: {
-      import: { rules: pluginImport.rules },
-    },
+    plugins: { import: { rules: pluginImport.rules } },
     rules: {
       'import/order': [
         'error',
